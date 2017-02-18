a768c8a1b8ad182675ea6a10b821299583c29bb0
MateoMat <m.maslank@interia.pl>
2017-02-18 16:31:32 +0100

    FOS Group functiona; added

--- src/href/hrefBundle/Entity/User.php
+++ src/href/hrefBundle/Entity/User.php
@@ -4,6 +4,8 @@ namespace href\hrefBundle\Entity;
 
 use FOS\UserBundle\Entity\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
+use href\hrefBundle\Entity\Group;
+
 
 /**
  * @ORM\Entity
@@ -23,4 +25,15 @@ class User extends BaseUser
         parent::__construct();
         // your own logic
     }
+    
+    /**
+     * @ORM\ManyToMany(targetEntity="Group")
+     * @ORM\JoinTable(name="fos_user_user_group",
+     *      joinColumns={@ORM\JoinColumn(name="user_id", referencedColumnName="id")},
+     *      inverseJoinColumns={@ORM\JoinColumn(name="group_id", referencedColumnName="id")}
+     * )
+     */
+    protected $groups;
+    
+    
 }
