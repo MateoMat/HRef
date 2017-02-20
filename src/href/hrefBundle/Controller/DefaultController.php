f44233c4f63d8e202aa01e2d4e1783994519d894
MateoMat <m.maslank@interia.pl>
2017-02-20 14:28:07 +0100

    Update show all Users section

--- src/href/hrefBundle/Controller/DefaultController.php
+++ src/href/hrefBundle/Controller/DefaultController.php
@@ -4,6 +4,8 @@ namespace href\hrefBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use href\hrefBundle\Entity\User;
+use href\hrefBundle\Repository;
 
 class DefaultController extends Controller
 {
@@ -14,4 +16,19 @@ class DefaultController extends Controller
     {
         return $this->render('hrefBundle:Default:index.html.twig');
     }
+    
+    /**
+     * @Route("/showAllUsers",name="showAllUsers")
+     */
+    public function showAllUsersAction()
+            {
+        
+        $userManager=$this->get('fos_user.user_manager');
+        $users=$userManager->findUsers();
+        
+        return $this->render('hrefBundle:Default:showAllUsers.html.twig',array('users'=>$users));
+        
+        
+    }
+    
 }
